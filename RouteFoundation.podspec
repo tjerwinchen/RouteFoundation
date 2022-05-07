1315cc65f16a7055b205185794beb032128b077e
Theo Chen <theo.chen@codebase.codes>
2022-05-07 23:22:22 +0800

    feat: add tooling script
    refact: fix various configurations

--- RouteFoundation.podspec
+++ RouteFoundation.podspec
@@ -1,31 +1,24 @@
-#
-#  Be sure to run `pod spec lint RouteFoundation.podspec' to ensure this is a
-#  valid spec and to remove all comments including this before submitting the spec.
-#
-#  To learn more about Podspec attributes see https://guides.cocoapods.org/syntax/podspec.html
-#  To see working Podspecs in the CocoaPods repo see https://github.com/CocoaPods/Specs/
-#
+enable_test = true
 
 Pod::Spec.new do |spec|
 
   spec.name         = "RouteFoundation"
   spec.version      = "0.0.1"
   spec.summary      = "An easy-to-use and lightweight Route in swift for UIKit."
-
-  spec.description  = <<-DESC
-An easy-to-use and lightweight Route in swift for UIKit
-                   DESC
-
-  spec.homepage     = "https://github.com/tjerwinchen/RouteFoundation"
+  spec.homepage     = "https://github.com/tjerwinchen/#{spec.name}"
   spec.license      = { :type => "MIT", :file => "LICENSE" }
   spec.author             = { "Theo Chen" => "theo.chen@codebase.codes" }
   spec.social_media_url   = "https://twitter.com/zhecui"
 
-  spec.ios.deployment_target = "13.0"
-  # spec.watchos.deployment_target = "2.0"
-  # spec.tvos.deployment_target = "9.0"
+  spec.swift_version = '5.6'
+  spec.ios.deployment_target = "12.0"
 
-  spec.source       = { :git => "https://github.com/tjerwinchen/RouteFoundation.git", :tag => "#{spec.version}" }
-  spec.source_files = "Sources/RouteFoundation/**/*.{h,m,swift}"
+  spec.source       = { :git => "#{spec.homepage}.git", :tag => "#{spec.version}" }
+  spec.source_files = "Sources/#{spec.name}/**/*.{h,m,swift}"
   
+  if enable_test
+    spec.test_spec 'Tests' do |test_spec|
+      test_spec.source_files = "Tests/#{spec.name}Tests/**/*.{h,m,swift}"
+    end
+  end
 end
---
+++ tooling.sh
@@ -0,0 +1,109 @@
+#! /usr/bin/env bash
+
+cd "$(dirname "$0")" || exit
+
+SWIFTFORMAT_FILE=~/.tooling/swiftformat/base
+SWIFTLINT_FILE=~/.tooling/swiftlint/base.yml
+VERBOSE=false
+OUTPUT=""
+
+function parse_args() {
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+    --verbose)
+      VERBOSE=true
+      # Shift past argument since there's no value expected.
+      shift
+      ;;
+    # Unknown option.
+    *)
+      # Save it in an array for later,
+      POSITIONAL+=("$1")
+      # Shift past argument.
+      shift
+      ;;
+    esac
+  done
+  # Restore positional parameters.
+  set -- "${POSITIONAL[@]}"
+}
+
+# swiftformat
+function format() {
+  if ! which swiftformat &> /dev/null; then
+    printf "⚠️  You don't have SwiftFormat installed locally. Learn more about SwiftFormat by visiting https://github.com/nicklockwood/SwiftFormat.\n"
+  else
+    if [ -f $SWIFTFORMAT_FILE ]; then
+      if $VERBOSE; then
+        swiftformat --config $SWIFTFORMAT_FILE . --verbose
+      else
+        OUTPUT+=$(swiftformat --config $SWIFTFORMAT_FILE .)
+      fi
+    else
+      printf "Not found $SWIFTLINT_FILE"
+      exit 1
+    fi
+  fi
+}
+
+# swiftlint
+function lint() {
+  if ! which swiftlint &> /dev/null; then
+    printf "⚠️  You don't have SwiftLint installed locally. Learn more about SwiftLint by visiting https://github.com/realm/SwiftLint.\n"
+    printf "ℹ️️  run \e[0;32minfra/developer-tools/libs/environment/swift.sh\e[0m under monorepo to install tooling\n"
+  else
+    if [ -f $SWIFTLINT_FILE ]; then
+      if $VERBOSE; then
+        swiftlint --no-cache --config $SWIFTLINT_FILE
+      else
+        OUTPUT+=$(swiftlint --no-cache --config $SWIFTLINT_FILE)
+      fi
+    else
+      printf "Not found $SWIFTLINT_FILE"
+      exit 1
+    fi
+  fi
+}
+
+function podlint() {
+  if ! which bundle &> /dev/null; then
+    printf "⚠️  You don't have SwiftLint installed locally.\n"
+    gem install bundler & bundle install
+    if $VERBOSE; then
+      bundle exec pod lib lint RouteFoundation.podspec --verbose
+    else
+      bundle exec pod lib lint RouteFoundation.podspec
+    fi
+  else
+    if $VERBOSE; then
+      bundle exec pod lib lint RouteFoundation.podspec --verbose
+    else
+      bundle exec pod lib lint RouteFoundation.podspec
+    fi
+  fi
+}
+
+function generate() {
+  xcodegen generate -s Example/Cocoapods/.project.yml --project Example/Cocoapods/
+  xcodegen generate -s Example/SPM/.project.yml --project Example/SPM/
+}
+
+function check() {
+  format
+  lint
+}
+
+if [[ $1 =~ ^(format|lint|check|podlint|generate)$ ]]; then
+  # Parsing common args
+  parse_args "$@"
+  "$@"
+#  if [ "$OUTPUT" ]; then
+#    echo "$OUTPUT" >&2
+#    exit 1
+#  fi
+else
+  echo "Invalid subcommand $1" >&2
+  exit 1
+fi
